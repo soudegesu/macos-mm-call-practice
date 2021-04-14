f7d68d3177857c3d11b157fc1fab4f0c921780e9
soudegesu <soudegesu@hotmail.co.jp>
2021-04-14 11:10:09 +0900

    initial commit

---
+++ macos-mm-call-practice/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  macos-mm-call-practice
+//
+//  Created by soudegesu on 2021/04/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
